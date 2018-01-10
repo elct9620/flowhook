784059c910991283e96ed44a7866b63a235a7c60
蒼時弦也 <elct9620@frost.tw>
2018-01-10 18:18:36 +0800

    Add daemonize support

--- lib/flowhook/worker.rb
+++ lib/flowhook/worker.rb
@@ -5,18 +5,82 @@ module Flowhook
   class Worker
     def initialize(options)
       @options = options
+      @pidfile = File.expand_path('flowhook.pid')
 
       initialize_stream
     end
 
     def start
+      check_pid
+      daemonize if daemonize?
+      write_pid
+      trap_signals
+
       @stream.read do |event|
         # TODO: Logging response
         Thread.new { send event }
       end
     end
 
+    def daemonize?
+      @options.daemonize
+    end
+
+    def pidfile?
+      !@pidfile.nil?
+    end
+
     private
+
+    # TODO: Refactor PID manager: https://codeincomplete.com/posts/ruby-daemons/
+    def check_pid
+      return unless pidfile?
+      case pid_status(@pidfile)
+      when :running, :not_owned
+        puts "Worker is running. Check #{@pidfile}"
+        exit 1
+      when :dead
+        File.delete(@pidfile)
+      end
+    end
+
+    def write_pid
+      return unless pidfile?
+      File.write(@pidfile, Process.pid)
+      at_exit { File.delete(@pidfile) if File.exist?(@pidfile) }
+    rescue Errno::EEXIST
+      check_pid
+      retry
+    end
+
+    def pid_status(pidfile)
+      return :exited unless File.exist?(pidfile)
+      pid = File.read(pidfile).to_i
+      return :dead if pid.zero?
+      Process.kill(0, pid)
+      :running
+    rescue Errno::ESRCH
+      :dead
+    rescue Errno::EPERM
+      :not_owned
+    end
+
+    def daemonize
+      exit if fork
+      Process.setsid
+      exit if fork
+      Dir.chdir '/'
+    end
+
+    def trap_signals
+      trap :QUIT do
+        @stream.stop!
+      end
+
+      trap :SIGINT do
+        @stream.stop!
+      end
+    end
 
     def send(event)
       use_ssl = uri.scheme == 'https'
